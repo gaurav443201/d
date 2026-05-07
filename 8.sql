667a542b11619e9f1c3e619b510392d9c64e3fda
CRAZYSHIT <NAVGHAREGAURAV80@GMAIL.COM>
2026-05-07 10:59:31 +0530

    Add CRUD operations for student records in CollegeDB
    This SQL script includes operations for creating, reading, updating, and deleting student records in the CollegeDB database.

---
+++ 8.sql
@@ -0,0 +1,28 @@
+// Create Database
+use CollegeDB
+
+// Create (Insert using save method)
+db.student.save({id:1, name:"Gaurav", marks:85})
+db.student.save({id:2, name:"Rahul", marks:90})
+db.student.save({id:3, name:"Amit", marks:70})
+
+// Read (All)
+db.student.find()
+
+// Read with condition (Logical Operator)
+db.student.find({$and:[{marks:{$gt:80}}, {marks:{$lt:100}}]})
+
+// Read with OR
+db.student.find({$or:[{name:"Gaurav"}, {marks:70}]})
+
+// Update
+db.student.updateOne({id:1}, {$set:{marks:95}})
+
+// Update multiple
+db.student.updateMany({marks:{$lt:80}}, {$set:{marks:80}})
+
+// Delete
+db.student.deleteOne({id:2})
+
+// Delete multiple
+db.student.deleteMany({marks:{$lt:80}})
